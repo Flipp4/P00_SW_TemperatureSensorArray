605fad3cc1f74c46acbc13189ea23709f4ccdbbe
Filip Pyszny <flip_p4@o2.pl>
2021-11-29 20:39:22 +0100

    [NEW] Added easy-named functions for BSP

---
+++ Drivers/BSP/BSP.c
@@ -0,0 +1,81 @@
+/*
+ * BSP.c
+ *
+ *  Created on: 29 lis 2021
+ *      Author: Filip
+ */
+
+#include "BSP.h"
+#include "main.h"
+
+
+void OperateLED_A (LEDState_t eState)
+{
+	if(eState == eLED_On)
+	{
+		HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, GPIO_PIN_SET);
+	}
+	else if(eState == eLED_Off)
+	{
+		HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, GPIO_PIN_RESET);
+	}
+}
+void OperateLED_B (LEDState_t eState)
+{
+	if(eState == eLED_On)
+	{
+		HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_SET);
+	}
+	else if(eState == eLED_Off)
+	{
+		HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_RESET);
+	}
+}
+void OperateLED_C (LEDState_t eState)
+{
+	if(eState == eLED_On)
+	{
+		HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_SET);
+	}
+	else if(eState == eLED_Off)
+	{
+		HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_RESET);
+	}
+}
+void OperateLED_D (LEDState_t eState)
+{
+	if(eState == eLED_On)
+	{
+		HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, GPIO_PIN_SET);
+	}
+	else if(eState == eLED_Off)
+	{
+		HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, GPIO_PIN_RESET);
+	}
+}
+
+void OperateSensorPower_A (SensorState_t eState)
+{
+	if(eState == eLED_On)
+	{
+		HAL_GPIO_WritePin(I2C1_VDD_GPIO_Port, I2C1_VDD_Pin, GPIO_PIN_SET);
+	}
+	else if(eState == eLED_Off)
+	{
+		HAL_GPIO_WritePin(I2C1_VDD_GPIO_Port, I2C1_VDD_Pin, GPIO_PIN_RESET);
+	}
+}
+void OperateSensorPower_B (SensorState_t eState)
+{
+	if(eState == eLED_On)
+	{
+		HAL_GPIO_WritePin(I2C2_VDD_GPIO_Port, I2C2_VDD_Pin, GPIO_PIN_SET);
+	}
+	else if(eState == eLED_Off)
+	{
+		HAL_GPIO_WritePin(I2C2_VDD_GPIO_Port, I2C2_VDD_Pin, GPIO_PIN_RESET);
+	}
+}
+
+
+
---
+++ Drivers/BSP/BSP.h
@@ -0,0 +1,34 @@
+/*
+ * BSP.h
+ *
+ *  Created on: 29 lis 2021
+ *      Author: Filip
+ */
+
+#ifndef BSP_BSP_H_
+#define BSP_BSP_H_
+
+typedef enum LEDState_t
+{
+	eLED_On,
+	eLED_Off,
+	eLED_StateCount
+}LEDState_t;
+
+typedef enum SensorState_t
+{
+	eSensor_On,
+	eSensor_Off,
+	eSensor_StateCount
+}SensorState_t;
+
+void OperateLED_A (LEDState_t eState);
+void OperateLED_B (LEDState_t eState);
+void OperateLED_C (LEDState_t eState);
+void OperateLED_D (LEDState_t eState);
+
+void OperateSensorPower_A (SensorState_t eState);
+void OperateSensorPower_B (SensorState_t eState);
+
+
+#endif /* BSP_BSP_H_ */
